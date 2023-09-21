426870bf85b5eae845cd1c61d64dbf92e83f8c3f
yamamine3 <131436956+yamamine3@users.noreply.github.com>
2023-09-21 11:55:18 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,14 @@ function setUserName() {
   localStorage.setItem("name", myName);
   myHeading.textContent = `Mozilla はかっこいいよ、${myName} さん`;
 }
-
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
 myButton.onclick = function() {
   setUserName();
 }
